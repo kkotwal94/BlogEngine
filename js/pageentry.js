9f03e82477f4a608f86cb7054c3d9457983b695d
Karan Kotwal <kotwalkaran64@gmail.com>
2016-02-03 07:30:24 +0000

    Alloyeditor

--- js/pageentry.js
+++ js/pageentry.js
@@ -10,7 +10,41 @@
              var container = document.getElementById('container');
              var fillingWithElement = document.getElementById(locPath);
             container.innerHTML = fillingWithElement.innerHTML;
+           /*
+            var editor = new MediumEditor('.editable', {
+    toolbar: {
+       
+        allowMultiParagraphSelection: true,
+        buttons: ['bold', 'italic', 'underline', 'anchor','h1', 'h2', 'h3', 'quote'],
+        diffLeft: 0,
+        diffTop: -10,
+        firstButtonClass: 'medium-editor-button-first',
+        lastButtonClass: 'medium-editor-button-last',
+        standardizeSelectionStart: false,
+        static: false,
+        relativeContainer: null,
+        align: 'center',
+        sticky: false,
+        updateOnEmptySelection: false
+    }
+            });
+        */
+        var editor = AlloyEditor.editable('editable', {
+    toolbars: {
+        styles: {
+            selections: [
+                {
+                    name: 'text',
+                    buttons: ['bold'],
+                    test: AlloyEditor.SelectionTest.text
+                }
+            ]
         }
+    }
+});
+        }
+        
+        
       };
       
       window.addEventListener("hashchange", handleHash);
