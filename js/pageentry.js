77cd3821914f7cc7b7d64f20ab8b0b81a9271a10
Karan Kotwal <kotwalkaran64@gmail.com>
2016-02-03 21:45:54 +0000

    Edited Toolbar

--- js/pageentry.js
+++ js/pageentry.js
@@ -29,9 +29,58 @@
     }
             });
         */
-        var editor = AlloyEditor.editable('editable');
+var Selections = [{
+        name: 'link',
+        buttons: ['linkEdit'],
+        test: AlloyEditor.SelectionTest.link
+    }, {
+        name: 'image',
+        buttons: ['imageLeft', 'imageRight'],
+        test: AlloyEditor.SelectionTest.image
+    }, {
+        name: 'text',
+        buttons: [{
+                        name: 'styles',
+                        cfg: {
+                            styles: [
+                                {
+                                    name: 'Head 1',
+                                    style: { element: 'h1' }
+                                },
+                                {
+                                    name: 'Head 2',
+                                    style: { element: 'h2' }
+                                },
+                                {
+                                    name: 'Code',
+                                    style: { element: 'code' }
+                                },
+                                
+                            ]
+                        }
+                    }, 'bold', 'italic', 'underline','quote', 'strike', 'link', 'ul', 'ol', 'twitter', 'subscript', 'superscript'],
+        test: AlloyEditor.SelectionTest.text
+    }, {
+        name: 'table',
+        buttons: ['tableRow', 'tableColumn', 'tableCell', 'tableRemove'],
+        getArrowBoxClasses: AlloyEditor.SelectionGetArrowBoxClasses.table,
+        setPosition: AlloyEditor.SelectionSetPosition.table,
+        test: AlloyEditor.SelectionTest.table
+}];
+
+        var editor = AlloyEditor.editable('editable', {
+    toolbars: {
+       add: {
+        buttons: ['image', 'camera', 'hline', 'table'],
+        tabIndex: 2
+    },
+        styles: {
+            selections: Selections
         }
-        
+    }
+});
+
+}
         
       };
       
--- js/render.js
+++ js/render.js
@@ -30,7 +30,57 @@ var renderPage = function(newURL){
     }
 });
 */
-  var editor = AlloyEditor.editable('editable');
+ var Selections = [{
+        name: 'link',
+        buttons: ['linkEdit'],
+        test: AlloyEditor.SelectionTest.link
+    }, {
+        name: 'image',
+        buttons: ['imageLeft', 'imageRight'],
+        test: AlloyEditor.SelectionTest.image
+    }, {
+        name: 'text',
+        buttons: [{
+                        name: 'styles',
+                        cfg: {
+                            styles: [
+                                {
+                                    name: 'Head 1',
+                                    style: { element: 'h1' }
+                                },
+                                {
+                                    name: 'Head 2',
+                                    style: { element: 'h2' }
+                                },
+                                {
+                                    name: 'Code',
+                                    style: { element: 'code' }
+                                },
+                                
+                            ]
+                        }
+                    }, 'bold', 'italic', 'underline','quote', 'strike', 'link', 'ul', 'ol', 'twitter', 'subscript', 'superscript'],
+        test: AlloyEditor.SelectionTest.text
+    }, {
+        name: 'table',
+        buttons: ['tableRow', 'tableColumn', 'tableCell', 'tableRemove'],
+        getArrowBoxClasses: AlloyEditor.SelectionGetArrowBoxClasses.table,
+        setPosition: AlloyEditor.SelectionSetPosition.table,
+        test: AlloyEditor.SelectionTest.table
+}];
+
+        var editor = AlloyEditor.editable('editable', {
+    toolbars: {
+       add: {
+        buttons: ['image', 'camera', 'hline', 'table'],
+        tabIndex: 2
+    },
+        styles: {
+            selections: Selections
+        }
+    }
+});
+
   //we are changing the links href to decide where we go!
   //nextPage.setAttribute("href", "page"+n);
 };
